3d7678ea82c44854181f5c5eeee4b068f415d45f
Petr Hanak <petr.nahak@gmail.com>
2018-03-20 23:02:08 +0100

    black logo without background

--- scripts/resizing.js
+++ scripts/resizing.js
@@ -1,31 +1,35 @@
-const clickAnimationTime = 200;
-const focusAnimationTime = 400;
-const clickResizeAmount = 200;
-const focusResizeAmount = 200;
+const clickResizeAmount = 20;
+const clickAnimationTime = 100;
+const focusResizeAmount = 70;
+const focusAnimationTime = 200;
+const gooutResizeAmount = 20;
+const gooutResizeTime = 200;
 //
 $(document).ready(function() {
-    $(".logo").click(function() {
-      $(".logo").animate({
-        height: '-=' + clickResizeAmount + 'px',
-        width: '-=' + clickResizeAmount + 'px',
-      },
-      clickAnimationTime);
-      $(".logo").animate({
-        height: '+=' + clickResizeAmount + 'px',
-        width: '+=' + clickResizeAmount + 'px',
-      },
-      300);
-    });
     $(".logo").mouseover(function() {
       $(".logo").animate({
         height: '+=' + focusResizeAmount + 'px',
-        width: '+=' + focusResizeAmount + 'px',
       }, focusAnimationTime);
     });
     $(".logo").mouseout(function() {
       $(".logo").animate({
         height: '-=' + focusResizeAmount + 'px',
-        width: '-=' + focusResizeAmount + 'px',
       }, focusAnimationTime);
+    });
+    $(".goout").mouseover(function() {
+      $(".goout").animate({
+        height: '+=' + gooutResizeAmount + 'px',
+        marginLeft: '-=' + gooutResizeAmount / 2 + 'px',
+        marginTop: '-=' + gooutResizeAmount / 2 + 'px',
+        borderRadius: 60 + "px",
+      }, gooutResizeTime);
+    });
+    $(".goout").mouseout(function() {
+      $(".goout").animate({
+        height: '-=' + gooutResizeAmount + 'px',
+        marginLeft: '+=' + gooutResizeAmount / 2 + 'px',
+        marginTop: '+=' + gooutResizeAmount / 2 + 'px',
+        borderRadius: 90 + "px",
+      }, gooutResizeTime);
     });
 });
--- scripts/rotate.js
+++ scripts/rotate.js
@@ -1,41 +1,58 @@
-
 <!--
-var x,y,n=0,ny=0,rotINT,rotYINT
-function rotateDIV()
-{
-x=document.getElementById("rotate2D")
-clearInterval(rotINT)
-rotINT=setInterval("startRotate()",1)
-}
+var x, z,
+  n = 0,
+  m = 0,
+  rotINT,
+  right = true
+  currentAngle = 0,
+  newAngle = 0;
 //
+function toggleRotate() {
+  x = document.getElementById("rotateToggle")
+  y = document.getElementById("rotateClock")
+  clearInterval(rotINT)
+  rotINT = setInterval("startRotate()", 1)
+}
+
 function startRotate() {
-  n=n+1
-  x.style.transform="rotate(" + n + "deg)"
-  x.style.webkitTransform="rotate(" + n + "deg)"
-  x.style.OTransform="rotate(" + n + "deg)"
-  x.style.MozTransform="rotate(" + n + "deg)"
-  if (n==90 || n==180 || n==270 || n==360) {
+  if(right) {
+    n = n + 1
+  } else {
+    n = n - 1
+  }
+  x.style.transform = "rotate(" + n + "deg)"
+  x.style.webkitTransform = "rotate(" + n + "deg)"
+  x.style.OTransform = "rotate(" + n + "deg)"
+  x.style.MozTransform = "rotate(" + n + "deg)"
+
+  if (n == 90) {
     clearInterval(rotINT)
-      if (n==360) {n=0}
+    right = false;
+  } else if (n == 0) {
+    clearInterval(rotINT)
+    right = true;
   }
 }
-//
-function rotateYDIV() {
-  y=document.getElementById("rotate3D")
-  clearInterval(rotYINT)
-  rotYINT=setInterval("startYRotate()",10)
+
+function clockTime() {
+  y = document.getElementById("rotateClock")
+  clearInterval(rotINT)
+  newAngle = m + Math.floor(Math.random() * 180) + 10;
+  rotINT = setInterval("clockTick()", 1)
 }
-//
-function startYRotate()
-{
-ny=ny+1
-y.style.transform="rotateY(" + ny + "deg)"
-y.style.webkitTransform="rotateY(" + ny + "deg)"
-y.style.OTransform="rotateY(" + ny + "deg)"
-y.style.MozTransform="rotateY(" + ny + "deg)"
-if (ny==180 || ny>=360) {
-  clearInterval(rotYINT)
-    if (ny>=360){ny=0}
+
+function clockTick() {
+  m = m + 1
+  y.style.transform = "rotate(" + m + "deg)"
+  y.style.webkitTransform = "rotate(" + m + "deg)"
+  y.style.OTransform = "rotate(" + m + "deg)"
+  y.style.MozTransform = "rotate(" + m + "deg)"
+
+  if(m == newAngle) {
+    clearInterval(rotINT)
+      if (m >= 360) {
+        m = m - 360
+      }
   }
 }
 //-->
